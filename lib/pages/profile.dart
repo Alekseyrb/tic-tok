bd59158b596a049b1cf30ce64de53848eeff2898
Aleksey <kravchenko.a.a.rb@gmail.com>
2022-07-26 13:32:21 +0300

    add profile

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,14 +1,262 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:tik_tok/util/profiletab_one.dart';
+import 'package:tik_tok/util/profiletab_three.dart';
+import 'package:tik_tok/util/profiletab_two.dart';
 
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('PROFILE')),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Aleksey',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Icon(
+            Icons.person_add,
+            color: Colors.black,
+          ),
+          actions: const [
+            Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: const [
+            IconNameUser(),
+            InfoUser(),
+            SizedBox(
+              height: 15,
+            ),
+            ButtonsEditProfile(),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Information',
+              style: TextStyle(color: Colors.grey),
+            ),
+            TabBarWidget(),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  FirstTab(),
+                  SecondTab(),
+                  ThirdTab(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IconNameUser extends StatelessWidget {
+  const IconNameUser({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 120,
+          width: 120,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey[300],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            'Aleksey',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class InfoUser extends StatelessWidget {
+  const InfoUser({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Container(
+            alignment: Alignment.centerRight,
+            child: Column(
+              children: const [
+                Text(
+                  '37',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Following',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            child: Column(
+              children: const [
+                Text(
+                  '8',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Followers',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              children: const [
+                Text(
+                  '124',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Likes',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonsEditProfile extends StatelessWidget {
+  const ButtonsEditProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              'Edit profile',
+              style: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade300),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Icon(
+            Icons.bookmark_outline,
+            color: Colors.grey[800],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TabBarWidget extends StatelessWidget {
+  const TabBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      tabs: const [
+        Tab(
+          icon: Icon(
+            Icons.grid_3x3,
+            color: Colors.black,
+          ),
+        ),
+        Tab(
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.black,
+          ),
+        ),
+        Tab(
+          icon: Icon(
+            Icons.lock_outline_rounded,
+            color: Colors.black,
+          ),
+        ),
+      ],
     );
   }
 }
